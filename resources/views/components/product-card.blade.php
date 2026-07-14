81be6f2dde09a1fa770b27dc7d02da884b743f00
nass3er <n716527766@gmail.com>
2026-07-14 15:01:25 +0300

    fix image product

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -1,11 +1,11 @@
-<div class="relative bg-transparent rounded-xl overflow-hidden  max-w-xs " data-aos="fade-up"
+<div class="relative bg-transparent rounded-xl overflow-hidden w-full max-w-xs" data-aos="fade-up"
     data-aos-once="true" data-aos-duration="700">
     {{-- Gray bottom background layer (half card height) --}}
     <div class="absolute bottom-0 left-0 w-full h-1/2 z-0"></div>
 
     {{-- Inner wrapper for hover transition --}}
     <div
-        class="relative z-10 flex flex-col items-center text-center p-4 transition-transform duration-500 ease-in-out hover:scale-105">
+        class="relative z-10 flex flex-col items-center text-center p-4 transition-transform duration-500 ease-in-out hover:scale-105 w-full">
         {{-- Image section: stands out, partially over gray --}}
 
 
